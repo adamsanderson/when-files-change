8b46ad08f09750b749fc90c61c640a212710784c
adamsanderson <netghost@gmail.com>
2013-04-27 21:43:12 -0700

    Add verbose mode (-v) to change listener.

--- lib/change_listener.rb
+++ lib/change_listener.rb
@@ -5,12 +5,15 @@ class ChangeListener
   attr_reader :listener
   attr_reader :command
   attr_reader :arguments
+  attr_accessor :verbose
+  attr_accessor :out
   
   def initialize(command, *arguments)
     @command   = command
     @arguments = arguments
-    @listener  = Listen.to('./').ignore(/^\.\w+/)
+    @out       = STDERR
     
+    @listener  = Listen.to('./').ignore(/^\.\w+/)
     @listener.change{ execute }
   end
   
@@ -20,22 +23,37 @@ class ChangeListener
   
   def execute
     listener.pause
-    success = Kernel.system(ENV, command, *arguments)
-    report_error unless success
+    run_command
     listener.unpause
   end
   
   def ignore(*regexps)
-    @listener.ignore(*regexps)
+    listener.ignore(*regexps)
   end
   
   def ignore_path(*paths)
     globs = paths.map{|p| FileGlob.new(p)}
     
-    @listener.ignore(*globs)
+    listener.ignore(*globs)
+  end
+  
+  def verbose?
+    verbose
+  end
+  
+  private
+  
+  def run_command
+    out.print "#{command_to_s} " if verbose?
+    success = Kernel.system(ENV, command, *arguments)
+    report_error unless success
   end
   
   def report_error
-    STDERR.puts "#{command.inspect} exited with #{$?.exitstatus}"
+    out.puts "exited with #{$?.exitstatus}"
+  end
+  
+  def command_to_s
+    "#{command} #{arguments.join(' ')}"
   end
 end
